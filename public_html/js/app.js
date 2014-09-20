7fc115ef0fc43a486615b150ff5b0dceea94e34c
Michael Lavina <unknownshenronz@gmail.com>
2014-09-20 17:51:32 -0400

    Added time passage functionality

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,18 +1,21 @@
-(function(){
-    
+(function() {
+
     var app = angular.module('soleGuardian', []);
-    
-    app.controller('ResourceController', ['$scope', function($scope){
-        $scope.salary = 1000;
-        $scope.dateTime = 946702800000;
-        
-        this.elapseTime = function(timeElapsed) {
-            $scope.dateTime += timeElapsed;
-        };
-    }]);
-    
-    app.controller('TestController', ['$scope', function($scope) {
-        $scope.salary -= 200;
-    }]);
+    app.controller('ResourceController', ['$scope', function($scope) {
+            $scope.salary = 1000;
+            $scope.dateTime = 946702800000;
+
+            this.elapseTime = function(timeElapsed) {
+                $scope.dateTime += timeElapsed;
+            };
+
+            function step() {
+                $scope.$apply(function() {
+                    $scope.dateTime += 50;
+                })
+            }
+
+            var timer = setInterval(step, 20);
+        }]);
 })();
 
