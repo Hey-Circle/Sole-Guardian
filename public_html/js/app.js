c20d5d5a768fe32ab1180a55f64414f2671bbd53
Michael Lavina <unknownshenronz@gmail.com>
2014-09-20 19:56:37 -0400

    Work buttons disabled when working

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -3,19 +3,35 @@
     var app = angular.module('soleGuardian', []);
     app.controller('ResourceController', ['$scope', function($scope) {
             $scope.salary = 1000;
-            $scope.dateTime = 946702800000;
+            
+            $scope.dateTime = new Date(1993, 8,25, 8,30).getTime();
+            var timeRate = 50;
+            var endTime = 0;
+            $scope.working = false;
 
-            this.elapseTime = function(timeElapsed) {
-                $scope.dateTime += timeElapsed;
+            this.workTime = function(timeElapsed) {
+                endTime = $scope.dateTime + timeElapsed;
+                timeRate = 100000;
+                $scope.working = true;
+                
             };
 
             function step() {
                 $scope.$apply(function() {
-                    $scope.dateTime += 50;
+                    if($scope.working && endTime < $scope.dateTime){
+                        $scope.working  = false;
+                        timeRate = 50;
+                    }
+                    $scope.dateTime += timeRate;
+
                 })
             }
 
             var timer = setInterval(step, 20);
+            
+            
         }]);
+    
+    
 })();
 
